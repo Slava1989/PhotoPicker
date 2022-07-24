c4601e9f7581b71788ec70ba64e3775bbf637f6b
Veaceslav Chirita <veaceslav.chirita@devexperts.com>
2022-07-24 17:52:27 +0300

    First commit

--- PhotoPicker/ContentView.swift
+++ PhotoPicker/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+struct PhotosView: View {
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [.init(.adaptive(minimum: 100, maximum: .infinity), spacing: 3)], spacing: 3) {
+                ForEach(1..<11) { index in
+                    Image("beach_\(index)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(
+                            minWidth: 0,
+                            maxWidth: .infinity,
+                            minHeight: 0,
+                            maxHeight: .infinity)
+                        .clipped()
+                        .aspectRatio(1, contentMode: .fit)
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        PhotosView()
     }
 }
 
