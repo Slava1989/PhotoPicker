90304652dcfb5656fbb4414b6be6b9c4955530ee
Veaceslav Chirita <veaceslav.chirita@devexperts.com>
2022-07-24 17:14:34 +0300

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Veaceslav Chirita on 24.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
